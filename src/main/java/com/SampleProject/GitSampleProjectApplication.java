f345e0b4e95aa51691c2d702cf471ecad61444ca
unknown <crchandhurajesh13422@gmail.com>
2025-10-06 22:00:26 +0530

    adding my project

---
+++ src/main/java/com/SampleProject/GitSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.SampleProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleProjectApplication.class, args);
+	}
+
+}
